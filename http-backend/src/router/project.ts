3a03588ad1e2252ca664b340899f49bab2bc9680
arre_ankit <arre.ankit@gmail.com>
2025-03-06 01:29:01 +0530

    url

--- http-backend/src/router/project.ts
+++ http-backend/src/router/project.ts
@@ -69,7 +69,7 @@ router.post("/create", async (req,res): Promise<any> => {
     
       //Enqueue the GitHub indexing job – this runs in the background.
       await qstashClient.publishJSON({
-        url: `http://localhost:4000/v1/api/background/index-github`,
+        url: `https://pullscan.onrender.com/v1/api/background/index-github`,
         body: {
           "projectId": project.id,
           "githubUrl": zreq.data?.githubUrl || '',
